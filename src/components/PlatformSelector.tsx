d620e9097efb5c9f39b15771def06024b978c4ad
Liran Anteby <liranan@post.bgu.ac.il>
2023-11-23 17:23:33 +0200

    implement search

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -18,14 +18,16 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
-          <MenuItem
-            onClick={() => onSelectPlatform(platform)}
-            key={platform.id}
-          >
-            {platform.name}
-          </MenuItem>
-        ))}
+        {data.map((platform) => {
+          return (
+            <MenuItem
+              onClick={() => onSelectPlatform(platform)}
+              key={platform.id}
+            >
+              {platform.name}
+            </MenuItem>
+          );
+        })}
       </MenuList>
     </Menu>
   );
