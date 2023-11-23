77ca6dfaa37c8cc99bd6ab94388d76ebf7f18e39
Liran Anteby <liranan@post.bgu.ac.il>
2023-11-23 12:15:27 +0200

    display genres

--- src/components/GenraList.tsx
+++ src/components/GenraList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenraList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((gen) => (
-        <li key={gen.id}>{gen.name}</li>
+        <ListItem key={gen.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(gen.image_background)}
+            />
+            <Text fontSize="lg">{gen.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
